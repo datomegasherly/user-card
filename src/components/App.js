e3f3b96a8e567c2c547557f46b762dfc114bf482
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-10 08:38:28 +0330

    add bootstrap css , Filter component to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Filter from './Filter';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class App extends Component {
     render() {
         return (
             <div data-test="app-component">
-                
+                <Filter />
             </div>
         )
     }
