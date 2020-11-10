49c43b13d6ae5290a28e326c3f967ce71537e5f0
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-10 07:58:14 +0330

    add searchReducer , userReducer and usersReducer to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,42 @@
 import { combineReducers } from 'redux';
+import actionTypes from '../actionTypes';
 
-const startReducer = (state = null, action) => {
+/**
+ * keep search field in filter component to filter users
+ * @param {string} state 
+ * @param {object} action 
+ */
+const searchReducer = (state = '', action) => {
+    switch(action.type){
+        case actionTypes.SEARCH_USER:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+/**
+ * get a user data to edit or remove
+ * @param {object} state 
+ * @param {object} action 
+ */
+const userReducer = (state = {}, action) => {
+    switch(action.type){
+        case actionTypes.SELECT_USER:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+/**
+ * get list of users
+ * @param {array} state 
+ * @param {object} action 
+ */
+const usersReducer = (state = [], action) => {
     return state;
 }
 export default combineReducers({
-    startReducer
+    search: searchReducer,
+    user: userReducer,
+    users: usersReducer
 });
