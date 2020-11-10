832e837253f1f4dd924739deefc4996441f9d1ce
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-10 07:57:20 +0330

    add actionTypes for filter , select , edit and delete user

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,5 +1,8 @@
 const actionTypes = {
-    
+    SEARCH_USER: 'SEARCH_USER',
+    SELECT_USER: 'SELECT_USER',
+    EDIT_USER: 'EDIT_USER',
+    DELETE_USER: 'DELETE_USER'
 }
 
 export default actionTypes;
