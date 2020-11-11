03930cdccf280b6f8061389baa1245683b4515fc
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-11 09:53:07 +0330

    add getUsers action to update users reducer state by fetch data from jsonplaceholder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,30 @@
 import actionTypes from '../actionTypes';
+import axios from 'axios';
 
+/**
+ * update search user text
+ * @param {string} search 
+ */
 const searchUser = search => {
     return {
         type: actionTypes.SEARCH_USER,
         payload: search
     }
 }
+/**
+ * get Users data from jsonplaceholder
+ */
+const getUsers = () => {
+    return async(dispatch) => {
+        let users = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: actionTypes.USER_LIST,
+            payload: users && users.data ? users.data : []
+        });
+    }
+}
 
 export {
-    searchUser
+    searchUser,
+    getUsers
 }
