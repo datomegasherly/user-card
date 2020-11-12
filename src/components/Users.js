a2c4fc2d4a4768e6ab92645e4730ec19fa86d0fa
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-12 12:27:10 +0330

    add details button and link to user component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Users extends Component {
     render() {
@@ -10,14 +11,17 @@ class Users extends Component {
                 {
                     users && users.length && users.map(user => {
                         return (
-                            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pr-1 pl-1">
+                            <div key={user.username} className="col-xs-12 col-sm-6 col-md-4 col-lg-3 pt-2 pr-1 pl-1">
                                 <div className="card h6">
-                                    <div className="card-header pl-1">
+                                    <div className="card-header pl-1 pr-2">
                                         <svg width="1.25em" height="1.25em" viewBox="0 0 16 16" className="bi bi-award" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l-1.51-.229L8 1.126l-1.355.702-1.51.229-.684 1.365-1.086 1.072L3.614 6l-.25 1.506 1.087 1.072.684 1.365 1.51.229L8 10.874l1.356-.702 1.509-.229.684-1.365 1.086-1.072L12.387 6l.248-1.506-1.086-1.072-.684-1.365z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
                                         </svg>
                                         <b> {user.username}</b>
+                                        <div className="float-right">
+                                            <Link to={`/user/${user.id}`}><button className="btn btn-info btn-sm">Details</button></Link>
+                                        </div>
                                     </div>
                                     <div className="m-2">
                                         <div>Name :</div>
