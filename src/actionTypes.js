b4a1fad270fcc449b17c3dbdd770701ad7a0b6dd
datomegasherly <dat.omega.sherly@gmail.com>
2020-11-16 14:10:04 +0330

    add CREATE_USER to actionTypes

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -3,6 +3,7 @@ const actionTypes = {
     SEARCH_USER: 'SEARCH_USER',
     SELECT_USER: 'SELECT_USER',
     EDIT_USER: 'EDIT_USER',
+    CREATE_USER: 'CREATE_USER',
     DELETE_USER: 'DELETE_USER'
 }
 
